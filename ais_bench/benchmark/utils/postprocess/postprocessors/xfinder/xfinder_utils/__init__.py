3aeddbe8cf5189db6c1cd4c29e0d2c1818806e30
zhongzhoutan <cqyzdp1@163.com>
2025-11-25 20:09:08 +0800

    refactor: delete unused xfinder and navie resource

--- ais_bench/benchmark/utils/postprocess/postprocessors/xfinder/xfinder_utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from ais_bench.benchmark.utils.postprocess.postprocessors.xfinder.xfinder_utils.convert_data import *  # noqa
-from ais_bench.benchmark.utils.postprocess.postprocessors.xfinder.xfinder_utils.data_process import *  # noqa
-from ais_bench.benchmark.utils.postprocess.postprocessors.xfinder.xfinder_utils.PROMPT_TEMPLATE import *  # noqa
